922a6c875bb5d5566e60ebd4ca2344dfcfef667b
matteoferla <matteo.ferla@gmail.com>
2022-01-05 13:07:15 +0000

    :pencil: badges and better pypi page stuff

--- setup.py
+++ setup.py
@@ -23,9 +23,21 @@ if not util.find_spec('pyrosetta'):
 if not util.find_spec('pymol2'):
     warn('The module pymol2 is optionally required (conda or apt-get installable).')
 
+long_description = '''
+Scaffold hopping between bound compounds by stitching them together like a reanimated corpse.
+<img src="https://github.com/matteoferla/Fragmenstein/blob/master/images/fragmenstein.jpg?raw=true" width="300px">
+
+Documentation in [GitHub](https://github.com/matteoferla/Fragmenstein).
+
+[![colab demo](https://img.shields.io/badge/Run--demo--in--colab-colab_fragmenstein.ipynb-f9ab00?logo=googlecolab)](https://colab.research.google.com/github/matteoferla/Fragmenstein/blob/master/colab_fragmenstein.ipynb)
+
+![Ox](https://upload.wikimedia.org/wikipedia/en/thumb/2/2f/University_of_Oxford.svg/132px-University_of_Oxford.svg.png)
+'''
+
 setup(
     name='Fragmenstein',
     version='0.6.13',
+    python_requires='>=3.7',
     packages=find_packages(),
     include_package_data=True,
     package_data={'fragmenstein.mpro.data': ['template.pdb'],
@@ -36,7 +48,19 @@ setup(
     license='MIT',
     author='Matteo Ferla',
     author_email='matteo.ferla@gmail.com',
+    classifiers=[ # https://pypi.org/classifiers/
+        'Development Status :: 4 - Beta', # Development Status :: 5 - Production/Stable
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering :: Bio-Informatics',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
     description='Scaffold hopping between bound compounds by stitching them together like a reanimated corpse',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     entry_points={
         'console_scripts': ['fragmenstein=fragmenstein.cli:main'],
     }
