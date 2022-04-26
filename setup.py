4df6bccce2384d8b9b31d4acd13c352550aa6e34
matteoferla <matteo.ferla@gmail.com>
2022-04-26 09:34:12 +0100

    :arrow_up: subversion up due to tweaks

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ Documentation in [GitHub](https://github.com/matteoferla/Fragmenstein).
 
 setup(
     name='Fragmenstein',
-    version='0.8.2',
+    version='0.8.3',
     python_requires='>=3.7',
     packages=find_packages(),
     include_package_data=True,
