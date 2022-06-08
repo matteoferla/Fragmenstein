0f8c88ddcf411603cb8bd82ee3c985fcdfa3090d
matteoferla <matteo.ferla@gmail.com>
2022-06-08 12:36:49 +0100

    :bug: test was set up weirdly

--- test.py
+++ test.py
@@ -1,13 +1,13 @@
 import unittest
-from tests.internals import Internals
-from tests.problems import UnresolvedProblems
-from tests.mapping import Mappings
-from tests.monster_place import MonsterPlaceTests
-from tests.monster_combine import MonsterCombineTests
-from tests.MPro_combine import VictorCombineTests
-from tests.MPro_place import MProPlaceTester
-from tests.multivictor import MultivictorPlaceTests
-from tests.walton import WaltonTests
+from tests import Internals
+from tests import UnresolvedProblems
+from tests import Mappings
+from tests import MonsterPlaceTests
+from tests import MonsterCombineTests
+from tests import VictorCombineTests
+from tests import MProPlaceTester
+from tests import MultivictorPlaceTests
+from tests import WaltonTests
 
 # Todo: add a class to test missing modules.
 if __name__ == '__main__':
