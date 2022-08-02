5d7f39243693be48191cb8aab5c138d91afc0033
matteoferla <matteo.ferla@gmail.com>
2022-08-02 15:36:43 +0100

    :bug: version is off

--- fragmenstein/__init__.py
+++ fragmenstein/__init__.py
@@ -8,7 +8,7 @@ __author__ = "Matteo Ferla. [Github](https://github.com/matteoferla)"
 __email__ = "matteo.ferla@gmail.com"
 __date__ = "2022 A.D."
 __license__ = "MIT"
-__version__ = "0.7"
+__version__ = "0.9.9"
 __citation__ = ""
 
 ########################################################################################################################
