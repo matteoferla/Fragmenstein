26a2b35e628afdf645d7eb9da4231032263c85ba
matteoferla <matteo.ferla@gmail.com>
2023-01-11 12:51:29 +0000

    :hammer: useful fun

--- fragmenstein/version.py
+++ fragmenstein/version.py
@@ -1,1 +1,15 @@
-__version__ = "0.9.12"
+__version__ = "0.9.12.1"
+
+from typing import Dict
+def get_versions() -> Dict[str, str]:
+    """
+    Return a dict of versions of os, python, fragmenstein etc.
+    """
+    import pkg_resources, sys, platform
+
+    get_version = lambda name: pkg_resources.get_distribution(name).version
+
+    return dict(python=sys.version, os_type=platform.system(), arc=platform.machine(),
+                fragmenstein=get_version("fragmenstein"),
+                pyrosetta=get_version("pyrosetta")
+                )
