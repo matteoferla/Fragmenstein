e70dfada1260809fa149d4d62f34ab25182f4b84
matteoferla <matteo.ferla@gmail.com>
2023-09-21 16:11:35 +0100

    :pencil: subversion up

--- fragmenstein/version.py
+++ fragmenstein/version.py
@@ -1,4 +1,4 @@
-__version__ = "0.12.11"
+__version__ = "0.12.12"
 
 from typing import Dict
 def get_versions() -> Dict[str, str]:
--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ if not util.find_spec('pymol2'):
 
 setup(
     name='Fragmenstein',
-    version='0.12.11',
+    version='0.12.12',
     description='Merging, linking and placing compounds by stitching them together like a reanimated corpse',
     long_description=long_description,
     long_description_content_type='text/markdown',
